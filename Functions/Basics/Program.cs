56373c866737b6a1ac6497e3d37332bd8e60332f
hadi14250 <hadihk2k@gmail.com>
2024-10-25 15:00:07 +0400

    learnt and expirmented with functions in c#

---
+++ Functions/Basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+float MilesToKm(float miles) {
+    float result = miles * 1.6f;
+    return result;
+}
+
+void PrintWithPrefix(string thestr) {
+    Console.WriteLine($"::> {thestr}");
+}
+
+Console.WriteLine($"The result is {MilesToKm(8.0f)}");
+Console.WriteLine($"The result is {MilesToKm(52.0f)}");
+
+PrintWithPrefix("Test string");
+PrintWithPrefix("Another test string");
---
+++ Functions/DefaultParams/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+void PrintWithPrefix(string thestr, string prefix="")
+{
+    Console.WriteLine($"{prefix} {thestr}");
+}
+
+PrintWithPrefix("Hello there!");
+PrintWithPrefix("Hello there!", ">: ");
+
+PrintWithPrefix(prefix: "% ", thestr: "Hello there!");
