722643a2010a418d275102ad2af8056a61c7de15
hadi14250 <hadihk2k@gmail.com>
2024-10-25 11:41:13 +0400

    learnt and tested various c sharp string operations

---
+++ strings/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_res;
+            string str1 = "Hello world from Abu Dhabi";
+            string str2 = "This is a small letter string";
+            string str3 = "THIS IS A CAPITAL LETTER STRING";
+            string[] strs = {"one", "two", "three"};
+
+            Console.WriteLine("Length of str1 is: {0} characters", str1.Length);
+            Console.WriteLine(str1[0]);
+            foreach (char c in str1) {
+                if (c == ' ') {
+                    Console.Write("_");
+                }
+                else if (Char.IsUpper(c)) {
+                    Console.Write(Char.ToLower(c));
+                }
+                else {
+                    Console.Write(c);
+                }
+            }
+            Console.WriteLine(";");
+
+            Console.WriteLine(String.Concat(strs));
+            Console.WriteLine(String.Join("_", strs));
+            Console.WriteLine(String.Join("", strs));
+            Console.WriteLine(String.Join("----", strs));
+
+            Console.WriteLine("String.Compare(str1, str2) -> {0}", String.Compare(str1, str2));
+            Console.WriteLine("String.Compare(str1, str1) -> {0}", String.Compare(str1, str1));
+            Console.WriteLine("String.Compare(str2, str1) -> {0}", String.Compare(str2, str1));
+
+            Console.WriteLine("str2.Equals(str3) -> {0}", str2.Equals(str3));
+            Console.WriteLine("str2.Equals(str3) -> {0}", str2.Equals(str2));
+
+            Console.WriteLine("str1.IndexOf('o') -> {0}", str1.IndexOf('o'));
+            Console.WriteLine("str1.IndexOf(\"from\") -> {0}", str1.IndexOf("Abu Dhabi"));
+
+            Console.WriteLine("str1.LastIndexOf('o') -> {0}", str1.LastIndexOf('o'));
+            Console.WriteLine("str1.LastIndexOf(\"from\") -> {0}", str1.LastIndexOf("from"));
+
+
+            str_res = str1.Replace("Abu Dhabi", "Dubai");
+            Console.WriteLine("str_res = str1.Replace(\"Abu Dhabi\", \"Dubai\") -> {0}", str_res);
+            Console.WriteLine("str_res.IndexOf(\"Abu Dhabi\") -> {0}", str_res.IndexOf("Abu Dhabi"));
+
+        }
+    }
+}
---
+++ variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int         i = 90;
+            float       f = 42.42f;
+            decimal     d = 33.01m;
+            bool        b = true;
+            char        c = 'c';
+            string      str = "hello!";
+            var         x = "20";
+            var         y = 202;
+            string[]    strs = {"hey", "hadi", "how", "you", "doing?"};
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", i, f, d, b, c, str, x, y);
+            Console.WriteLine(strs[4].Length);
+        }
+    }
+}
