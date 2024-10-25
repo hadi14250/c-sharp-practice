72bf668ab6a5d51b88c6c5e3ba573ce6af6d42bf
hadi14250 <hadihk2k@gmail.com>
2024-10-25 14:10:12 +0400

    learnt and expiremented with string interpolation

---
+++ stringInterpolation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string make = "Mercedes-Benz";
+            string model = "G Class";
+            int year = 2020;
+            float miles = 8_450.27f;
+            decimal price = 60_275.0m;
+
+
+            Console.WriteLine("This car is a {0} {1} {2}, with {3} miles and costs ${4}",
+                year, make, model, miles, price);
+
+            Console.WriteLine($"This car is a {year} {make} {{{model}}}, with {miles} miles and costs {price:C2}");
+
+            Console.WriteLine($"This car is a {year} {make} {{{model}}}, with {miles * 1.6:F2} kilometers and costs {price:C2}");
+        }
+    }
+}
