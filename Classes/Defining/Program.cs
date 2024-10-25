1fe0110100be4cce0efaefa6bdec0e31e0751053
hadi14250 <hadihk2k@gmail.com>
2024-10-25 16:48:13 +0400

    learnt about classess and properties in c#

---
+++ Classes/Defining/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Defining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+            Book b2 = new Book("The Grapes of Wrath", "John Steinbeck", 464);
+            
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
+        }
+    }
+}
---
+++ Classes/Modifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+
+            b1._name = "Aldous Huxley";
+            Console.WriteLine(b1.GetDescription());
+
+            b1.SetName("Grapes of Wrath");
+            b1.SetAuthor("John Steinbeck");
+            b1.SetPagecount(464);
+            Console.WriteLine(b1.GetDescription());
+        }
+    }
+}
